3082b7f156b92bfc392dcfe9bfc390de45fb5d24
thomasrosen <thomas.rosen@me.com>
2023-05-28 22:20:42 +0200

    show the recordings in a hours-per-day graph

--- website/build.js
+++ website/build.js
@@ -3,6 +3,57 @@ import YAML from 'yaml'
 
 import render_map from './render_map.js'
 
+function get_hours_graph(all_metadata) {
+
+  const all_hours = all_metadata
+    .reduce((acc, item) => {
+      const this_date = new Date(item.timestamp)
+      const hours = this_date.getHours()
+
+      if (!acc[hours]) {
+        acc[hours] = 0
+      }
+      acc[hours] += 1
+
+      return acc
+    }, {})
+
+  const max_per_hour = Math.max(...Object.values(all_hours))
+
+  const hours = new Array(24).fill(0)
+    .reduce((acc, _, index) => {
+      const hour = String(index).padStart(2, '0') // 00, 01, 02, ...
+      const amount = all_hours[index] || 0 // get the amount of recordings for this hour or 0 if there is no property in all_hours
+      acc.push({
+        hour,
+        amount,
+        percent: amount / max_per_hour, // normalize the amount of recordings for this hour to a value between 0 and 1
+      })
+
+      return acc
+    }, [])
+    .sort((a, b) => a.hour.localeCompare(b.hour))
+
+  return hours
+}
+
+function render_hours_graph(all_metadata) {
+  const hours = get_hours_graph(all_metadata)
+  const html = `
+    <h3>Recordings per hour of day</h3>
+    <p>The graph shows the amount of recordings per hour of day.</p>
+    <div class="hours_graph">
+      ${hours.map((hour) => `
+        <div class="hours_graph__bar" style="height: ${hour.percent * 100}%" title="${hour.amount} recordings at ${hour.hour}h">
+          <div class="hours_graph__bar__hour">${hour.hour}</div>
+          <div class="hours_graph__bar__amount">${hour.amount ? hour.amount : ''}</div>
+        </div>
+      `).join('')}
+    </div>
+  `
+  return html
+}
+
 function getsec(time) {
   const arr = time.split(':');
   return (+arr[0]) * 3600 + (+arr[1]) * 60 + (+arr[2]);
@@ -53,24 +104,24 @@ function save_new_index_html(recording_metadata_html) {
 function create_metadata_html() {
   console.info('✅ started build.js')
 
-  const data = load_metadata_from_files()
+  const all_metadata = load_metadata_from_files()
 
-  const points = data.map((item) => ({
+  const points = all_metadata.map((item) => ({
     latitude: item.geolocation.latitude,
     longitude: item.geolocation.longitude,
   }))
 
-  const amount_of_recordings = data.length
+  const amount_of_recordings = all_metadata.length
 
-  const what_tags = [...new Set(data.flatMap(item => item.what))]
+  const what_tags = [...new Set(all_metadata.flatMap(item => item.what))]
     .sort((a, b) => a.localeCompare(b))
 
-  const sum_length_of_all_recordings = data
+  const sum_length_of_all_recordings = all_metadata
     .reduce((acc, item) => acc + item.length, 0)
 
   const what_tags_metadata = what_tags
   .map((tag) => {
-    const items_with_tag = data.filter((item) => item.what.includes(tag))
+    const items_with_tag = all_metadata.filter((item) => item.what.includes(tag))
 
     const amount_of_recordings_with_tag = items_with_tag.length
 
@@ -97,6 +148,7 @@ function create_metadata_html() {
   const length_of_recordings_as_text = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0') } (${sum_length_of_all_recordings} seconds)`
 
   const svg_map = render_map(points)
+  const hours_graph = render_hours_graph(all_metadata)
 
   const html = `
     <p><strong>Amount of recordings</strong>: ${amount_of_recordings}</p>
@@ -118,6 +170,7 @@ function create_metadata_html() {
       Red = Recording / Black = Berlin / Grey = Potsdam
     </p>
     ${svg_map}
+    ${hours_graph}
   `
 
   console.info('✅ HTML generated')
