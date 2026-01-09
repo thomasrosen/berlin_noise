b3caa8c2d6b5851aac07e8f9096a16ff6a7472b0
thomasrosen <thomas.rosen@me.com>
2026-01-09 21:25:56 +0100

    correct map viewport

--- website/render_map.js
+++ website/render_map.js
@@ -1,5 +1,5 @@
-import geojson2svg from 'geojson-to-svg'
 import fs from 'fs'
+import geojson2svg from 'geojson-to-svg'
 
 const map_filepath_berlin = './geojson/berlin.geojson' // the path is relative to the package.json level
 const map_filepath_potsdam = './geojson/potsdam.geojson' // the path is relative to the package.json level
@@ -58,7 +58,19 @@ function get_map_json(points) {
     // ),
   ]
 
+  // boundaries for berlin+potsdam
+  const minLat = 52.29056
+  const minLng = 12.90615
+  const maxLat = 52.73088
+  const maxLng = 13.80193
+
   points = points
+    .filter(point =>
+      point.latitude >= minLat
+      && point.latitude <= maxLat
+      && point.longitude >= minLng
+      && point.longitude <= maxLng
+    )
     .map(point => ({
       type: 'Feature',
       properties: {
@@ -68,6 +80,7 @@ function get_map_json(points) {
       geometry: { coordinates: [point.longitude, point.latitude], type: "Point" }
     }))
 
+
   geojson_berlin.features = [
     ...fills,
     ...points,
@@ -76,24 +89,24 @@ function get_map_json(points) {
   return geojson_berlin
 }
 
-function render_map (points) {
+function render_map(points) {
   const scale = 10000
   const result = geojson2svg()
-  .type('style')
-  .styles({
-    'berlin': { fill: 'black', color: 'black', weight: 10, opacity: 1 },
-    'potsdam': { fill: '#555', color: '#555', weight: 10, opacity: 1 },
-    'water': { fill: '#fff', color: '#fff', weight: 0, opacity: 1 },
-    'grass': { fill: '#0f0', color: '#0f0', weight: 0, opacity: 0.6 },
-    'point': { fill: 'red', color: 'red', weight: 0, opacity: 1 },
-  })
-  .projection(function (coord) {
-    const x = coord[0] * scale * 0.6 // Scale the X axis to look more like a square
-    const y = coord[1] * scale * -1 // Invert the Y axis
-    return [x, y]
-  })
-  .data(get_map_json(points))
-  .render()
+    .type('style')
+    .styles({
+      'berlin': { fill: 'black', color: 'black', weight: 10, opacity: 1 },
+      'potsdam': { fill: '#555', color: '#555', weight: 10, opacity: 1 },
+      'water': { fill: '#fff', color: '#fff', weight: 0, opacity: 1 },
+      'grass': { fill: '#0f0', color: '#0f0', weight: 0, opacity: 0.6 },
+      'point': { fill: 'red', color: 'red', weight: 0, opacity: 1 },
+    })
+    .projection(function (coord) {
+      const x = coord[0] * scale * 0.6 // Scale the X axis to look more like a square
+      const y = coord[1] * scale * -1 // Invert the Y axis
+      return [x, y]
+    })
+    .data(get_map_json(points))
+    .render()
 
   // var converter = geojson2svg({});
   // console.log(converter)
